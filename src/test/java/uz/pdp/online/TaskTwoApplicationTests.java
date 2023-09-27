884954f5c9259621bdc7796379d5ca8c9e476ede
farrukhgulomov <frankywht@gmail.com>
2023-09-27 22:04:13 +0500

    first commit

---
+++ src/test/java/uz/pdp/online/TaskTwoApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.pdp.online;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskTwoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
